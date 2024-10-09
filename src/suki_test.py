a5e48659d4ec3df4ac915d040f922af20195d709
ksukriti08 <ksukriti08@gmail.com>
2024-10-08 22:47:14 -0400

    finished project 4 part 2

--- src/suki_test.py
+++ src/suki_test.py
@@ -2,9 +2,26 @@ from torch import nn
 import torch.nn.functional as F
 import torch
 import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
 import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
+from torch.utils.data import Dataset, ConcatDataset
+
+class CustomDataset(Dataset):
+    def __init__(self, images, labels):
+        self.images = images
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        label = self.labels[idx] 
+        image = TF.rgb_to_grayscale(image)
+        image = TF.normalize(image, mean=[0.5], std=[0.5])
+        return image, label
 
 
 transform = transforms.Compose(
@@ -15,8 +32,9 @@ transform = transforms.Compose(
 print("Start loading images")
 images = torch.load("/home/skushwaha/DLGroup2/src/tensor_images.pt")
 labels = torch.load("/home/skushwaha/DLGroup2/src/labels.pt")
+labels = labels.tolist()
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-dataset = torch.utils.data.TensorDataset(images, labels)
+dataset = CustomDataset(images.to('cpu'), labels)
 combined_dataset = torch.utils.data.ConcatDataset([trainset, dataset])
 print(images.shape)
 print(labels)
--- src/utils.py
+++ src/utils.py
@@ -6,12 +6,30 @@ import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+from torch.utils.data import Dataset, ConcatDataset
+import torchvision.transforms.functional as TF
 
+class CustomDataset(Dataset): # define sub class from torch data Dataset class to create custom dataset
+
+    # instructions to create custom dataset subclass found here: https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+    def __init__(self, images, labels):
+        self.images = images
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        label = self.labels[idx] 
+        image = TF.rgb_to_grayscale(image) # converts image to grayscale
+        image = TF.normalize(image, mean=[0.5], std=[0.5]) # normalizes image
+        return image, label
 
 class Pipeline:
     def __init__(self):
 
-        self.device = torch.device('cpu' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cpu' if torch.cuda.is_available() else 'cpu') # just use cpu
         print(self.device)
 
         transform = transforms.Compose(
@@ -22,9 +40,11 @@ class Pipeline:
         trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
         images = torch.load("/home/skushwaha/DLGroup2/src/tensor_images.pt")
         labels = torch.load("/home/skushwaha/DLGroup2/src/labels.pt")
-        dataset = torch.utils.data.TensorDataset(images, labels)
-        combined_dataset = torch.utils.data.ConcatDataset([trainset, dataset])
-        self.trainloader = torch.utils.data.DataLoader(combined_dataset, batch_size=32, shuffle=True, num_workers=2)
+        labels = labels.tolist() # converts label tensor to list
+        labels = [label - 1 for label in labels] # coverts labels 1-10 to 0-9 to match cifar labels
+        dataset = CustomDataset(images.to('cpu'), labels) # move everything to cpu
+        combined_dataset = torch.utils.data.ConcatDataset([trainset, dataset]) # concat cifar and custom dataset
+        self.trainloader = torch.utils.data.DataLoader(combined_dataset, batch_size=32, shuffle=True, num_workers=0) # pass combined dataset to trainloader
 
         testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
         self.testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False, num_workers=2)
